cbedb3612a1d5abe905db7881f649b9116c3f2b7
Jaemin Ryu <ryumin0421@gmail.com>
2026-09-19 16:23:20 +0900

    [init] 스프링 프로젝트

---
+++ Icelink/src/main/java/com/kosscchthon/Icelink/IcelinkApplication.java
@@ -0,0 +1,13 @@
+package com.kosscchthon.Icelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcelinkApplication.class, args);
+	}
+
+}
